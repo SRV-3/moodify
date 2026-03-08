ebc0975d7b9985e50a9e89aefb9a47e24c7a1b3d
Sourav <thakursourav778@gmail.com>
2026-03-08 19:54:22 +0530

    daily commit

--- Frontend/src/features/auth/components/Protected.jsx
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react'
 import { useAuth } from '../hooks/useAuth'
 import { Navigate } from 'react-router'
 
 function Protected({children}) {
     const { user, loading} = useAuth()
 
-    if(!user){
-      return  <Navigate to="/login"/>
-    }
     if(loading){
       return <h1>Loading...</h1>
     }
+
+    if(!user){
+      return  <Navigate to="/login"/>
+    }
+    
 
   return children
 }
 
 export default Protected
+
